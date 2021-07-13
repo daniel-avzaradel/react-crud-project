a501c965c4cef6f945bad08413304cdd3904dfab
Daniel Avzaradel <daniel.avzaradel@gmail.com>
2021-07-13 15:13:57 +0300

    added 'no-users' message if users.length == 0

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,30 +1,40 @@
-import React, { useContext } from 'react';
-import {GlobalContext} from '../context/GlobalState'
-import { Link } from 'react-router-dom';
-import {
-    ListGroup,
-    ListGroupItem,
-    Button
-} from 'reactstrap'
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+import { Link } from "react-router-dom";
+import { ListGroup, ListGroupItem, Button } from "reactstrap";
 
 function UserList() {
-    const {users, removeUser} = useContext(GlobalContext)
+  const { users, removeUser } = useContext(GlobalContext);
 
-    return (
-        <ListGroup className="mt-4">
-        {users.map((user, i) => {
-           return(
-            <ListGroupItem className="d-flex" key={i}>
+  return (
+    <ListGroup className="mt-4">
+      {users.length > 0 ? (
+        <>
+          {users.map((user, i) => {
+            return (
+              <ListGroupItem className="d-flex" key={i}>
                 <strong>{user.name}</strong>
                 <div className="ml-auto">
-                    <Link className="btn btn-warning" to={`/edit/${user.id}`}>Edit</Link>
-                    <Button onClick={() => removeUser(user.id)} color='danger' style={{marginLeft: '10px'}}>Delete</Button>
+                  <Link className="btn btn-warning" to={`/edit/${user.id}`}>
+                    Edit
+                  </Link>
+                  <Button
+                    onClick={() => removeUser(user.id)}
+                    color="danger"
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Delete
+                  </Button>
                 </div>
-            </ListGroupItem>
-           )
-        })}
-        </ListGroup>
-    )
+              </ListGroupItem>
+            );
+          })}
+        </>
+      ) : (
+        <h4 className="text-center">NO USERS</h4>
+      )}
+    </ListGroup>
+  );
 }
 
-export default UserList
+export default UserList;
